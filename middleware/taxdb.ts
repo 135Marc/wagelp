7986cf7f7348178f476177237ac81215fd87c2e1
135Marc <a79097@alunos.uminho.pt>
2023-05-21 01:29:01 +0000

    Added Dockerfiles and Compose File

--- middleware/taxdb.ts
+++ middleware/taxdb.ts
@@ -1,6 +1,6 @@
 import DatabaseConstructor, {Database} from "better-sqlite3";
 
-const db = new DatabaseConstructor("../database/tax_db");
+const db = new DatabaseConstructor("tax_db");
 
 export function query(sql:any,params:any) {
     return db.prepare(sql).all(params);
